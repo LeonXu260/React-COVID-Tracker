a48b4d3027f2fae990b78246307f95d82f8bb353
Leon Xu <dcwinfield362@gmail.com>
2022-05-02 21:09:11 -0700

    doc: add data to Table component

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Table.css";
 
-function Table() {
-  return <section></section>;
+function Table({ countries }) {
+  return (
+    <section className="table">
+      {countries.map(({ country, cases }) => (
+        <tr>
+          <td>{country}</td>
+          <td>
+            <strong>{cases}</strong>
+          </td>
+        </tr>
+      ))}
+    </section>
+  );
 }
 
 export default Table;
