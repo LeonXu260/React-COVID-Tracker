3ba8fa95c614badb88a97f1be4c15c8d60ea3f1d
Leon Xu <dcwinfield362@gmail.com>
2022-05-03 20:36:44 -0700

    fix: showDataOnMap method

--- src/util.js
+++ src/util.js
@@ -28,11 +28,11 @@ export const sortData = (data) => {
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 };
 
-export const showDataOnMap = (data, casesType = "cases") => {
+export const showDataOnMap = (data, casesType = "cases") =>
   data.map((country) => (
     <Circle
       center={[country.countryInfo.lat, country.countryInfo.long]}
-      fillOpacity={0.4}
+      fillOpacity={0.1}
       color={casesTypeColors[casesType].hex}
       fillColor={casesTypeColors[casesType].hex}
       radius={
@@ -40,23 +40,7 @@ export const showDataOnMap = (data, casesType = "cases") => {
       }
     >
       <Popup>
-        <div className="info-container">
-          <div
-            className="info-flag"
-            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
-          ></div>
-          <div className="info-name">{country.country}</div>
-          <div className="info-confirmed">
-            Cases: {numeral(country.cases).format("0,0")}
-          </div>
-          <div className="info-recovered">
-            Recovered: {numeral(country.recovered).format("0,0")}
-          </div>
-          <div className="info-deaths">
-            Deaths: {numeral(country.deaths).format("0,0")}
-          </div>
-        </div>
+        <h1>I am a popup</h1>
       </Popup>
     </Circle>
   ));
-};
